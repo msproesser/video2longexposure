6fadf27fe6fba26cfd120ab8e365ed32c5bb3d96
Matheus Sproesser <matheus.sproesser@gmail.com>
2022-08-25 00:07:42 +0100

    POC-5 refactories

--- src/merge.mjs
+++ src/merge.mjs
@@ -1,5 +1,6 @@
 import { hexPixel, hexVectorToFile } from './steps/helpers/jimp-helpers.mjs';
-import pureJimpMethod, { incrementalReducer, lightenReducer } from './steps/merge-frames/pure-jimp-strategy.mjs';
+import jimpGpuStrategy from './steps/merge-frames/jimp-gpu-strategy.mjs';
+import pureJimpStrategy, { incrementalReducer, lightenReducer } from './steps/merge-frames/pure-jimp-strategy.mjs';
 import {cleanAll, extractFrames} from './utils.mjs'
 
 
@@ -7,7 +8,7 @@ import {cleanAll, extractFrames} from './utils.mjs'
 const [videoFile, fps] = process.argv.slice(2);
 cleanAll()
 .then(() => extractFrames(videoFile, fps))
-.then(pureJimpMethod(lightenReducer))
+.then(jimpGpuStrategy)
 .then(hexPixel)
 .then(hexVectorToFile('./final.png'))
 .then(console.log)
